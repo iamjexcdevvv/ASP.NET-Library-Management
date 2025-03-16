292af4a4afeb0dc6e6f832aa0a0f2cac712d7a4e
iamjexcdevvv <85010916+iamjexcdevvv@users.noreply.github.com>
2025-03-16 20:25:48 +0800

    database setup

---
+++ Data/BookManagementContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Data
+{
+    public class BookManagementContext : DbContext
+    {
+        public BookManagementContext(DbContextOptions<BookManagementContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<BookEntries> Books { get; set; }
+    }
+}
