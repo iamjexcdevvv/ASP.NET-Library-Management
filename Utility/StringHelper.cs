ec5fe0d73d8287effec006194d39ff9559e39671
iamjexcdevvv <85010916+iamjexcdevvv@users.noreply.github.com>
2025-03-19 19:12:22 +0800

    added add & edit book features

---
+++ Utility/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LibraryManagement.Utility
+{
+    public static class StringHelper
+    {
+        public static string CleanInput(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            input = input.Trim();
+
+            input = Regex.Replace(input, @"\s+", " ");
+
+            return input;
+        }
+    }
+}
---
+++ Validator/DateNotInTheFuture.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Validator
+{
+    public class DateNotInTheFuture : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? objValue,
+                                                   ValidationContext validationContext)
+        {
+            if (objValue != null)
+            {
+                DateTime dateValue = Convert.ToDateTime(objValue);
+
+                if (dateValue.Date.Year > DateTime.Now.Year)
+                {
+                    return new ValidationResult("* Invalid date");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
